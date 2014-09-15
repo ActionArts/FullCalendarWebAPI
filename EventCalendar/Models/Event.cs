2716c4a9e7344660d512ce1f221277b99ce17667
Docon <Ric1shaw>
2014-09-14 21:38:19 -0400

    First commit of setup files

---
+++ EventCalendar/Models/Event.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EventCalendar.Models
+{
+    public class Event
+    {
+        public int? Id { get; set; }
+        public string Title { get; set; }
+        public DateTime StartDttm { get; set; }
+        public DateTime EndDttm { get; set; }
+        public bool AllDay { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ EventCalendar/Models/EventSource.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EventCalendar.Models
+{
+    public class EventSource
+    {
+        public Event[] Events { get; set; }
+        string Start { get; set; }
+        string End { get; set; }
+    }
+}
